d31a7032e95f0e03a7291aaa7358af038e533293
Linh Phuong Tra <phuonglinhtra92@gmail.com>
2022-10-06 23:17:07 +0200

    la til Comment

---
+++ bacit-dotnet.MVC/Repositories/Comment/DapperCommentRepository.cs
@@ -0,0 +1,28 @@
+﻿using bacit_dotnet.MVC.DataAccess;
+using bacit_dotnet.MVC.Entities;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using MySqlConnector;
+
+namespace bacit_dotnet.MVC.Repositories.Comment
+{
+    public class DapperCommentRepository : ICommentRepository
+    {
+        private readonly ISqlConnector sqlConnector;
+        //Constructor
+        public DapperCommentRepository(ISqlConnector sqlConnector)
+        {
+            this.sqlConnector = sqlConnector;
+
+        }
+
+        public List<CommentEntity> GetAll()
+        {
+           using (var connection = sqlConnector.GetDbConnection() as MySqlConnection)
+            {
+                var comments = connection.Query<CommentEntity>("SELECT * FROM SuggestionComment");
+                return comments.ToList();
+            }
+        }
+    }
+}
---
+++ bacit-dotnet.MVC/Repositories/Comment/ICommentRepository.cs
@@ -0,0 +1,9 @@
+﻿using bacit_dotnet.MVC.Entities;
+
+namespace bacit_dotnet.MVC.Repositories.Comment
+{
+    public interface ICommentRepository
+    {
+        public List<CommentEntity> GetAll();
+    }
+}
