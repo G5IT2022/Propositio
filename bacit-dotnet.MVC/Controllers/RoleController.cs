9ad186ec2b3ceacd4778e131191f81ad84a3a27b
Administrator <Administrator@DESKTOP-IB31KH7>
2022-10-06 14:53:47 +0200

    la til rolle

---
+++ bacit-dotnet.MVC/Controllers/RoleController.cs
@@ -0,0 +1,25 @@
+﻿using bacit_dotnet.MVC.Models;
+using bacit_dotnet.MVC.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bacit_dotnet.MVC.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IRoleRepository roleRepository;
+
+        public RoleController(ILogger<HomeController> logger, IRoleRepository roleRepository)
+        {
+            _logger = logger;
+            this.roleRepository = roleRepository;
+        }
+
+        public IActionResult Index()
+        {
+            RoleViewModel model = new RoleViewModel();
+            model.roles = roleRepository.GetAllRoles();
+            return View(model);
+        }
+    }
+}
---
+++ bacit-dotnet.MVC/Entities/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace bacit_dotnet.MVC.Entities
+{
+    public class RoleEntity
+    {
+        public int role_id { get; set; }
+        public string role_name { get; set; }
+    }
+}
