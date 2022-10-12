9bcadf533291c5012d6c42a45817124d37561d05
Administrator <Administrator@DESKTOP-IB31KH7>
2022-10-12 12:35:17 +0200

    La til side for å logge inn, har ikke fåtttil funksjonen enda, men det visuelle er der

---
+++ bacit-dotnet.MVC/Models/AccountViewModel.cs
@@ -0,0 +1,14 @@
+﻿using bacit_dotnet.MVC.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace bacit_dotnet.MVC.Models
+{
+    public class AccountViewModel
+    {
+        [Required(ErrorMessage = "Du glemte å skrive inn navn.")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "Husk å skrive passord!")]
+        public string password { get; set; }
+    }
+}
