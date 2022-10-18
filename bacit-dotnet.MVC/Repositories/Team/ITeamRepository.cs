5580b5566be67b705f2c75e6651e64903ec0cd26
Gustav Svartsund <gustavsv@uia.no>
2022-10-18 11:10:39 +0200

    la til filene mine

--- bacit-dotnet.MVC/Repositories/Team/ITeamRepository.cs
+++ bacit-dotnet.MVC/Repositories/Team/ITeamRepository.cs
@@ -4,10 +4,17 @@ namespace bacit_dotnet.MVC.Repositories.Team
 {
     public interface ITeamRepository
     {
-        List<TeamEntity> GetALl();
+        List<TeamEntity> GetAll();
 
         //Henter en liste over alle teams for en ansatt
         List<TeamEntity> Get(int id);
+        //henter en listen over alle employees
+        List<EmployeeEntity> Get(string name);
 
+        List<EmployeeEntity> GetAll();
+
+       
     }
+
+   
 }
