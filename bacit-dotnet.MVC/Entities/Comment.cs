466382b0d2ea27293248df210b4759000e37420f
Krister <89388846+springvalley@users.noreply.github.com>
2022-10-19 09:23:30 +0200

    Update Comment.cs

--- bacit-dotnet.MVC/Entities/Comment.cs
+++ bacit-dotnet.MVC/Entities/Comment.cs
@@ -9,7 +9,7 @@ namespace bacit_dotnet.MVC.Entities
         public int emp_id { get; set; }
         public int suggestion_id { get; set; }
         public string description { get; set; }
-        public TimestampEntity timestamp { get; set; }
+        public DateTime timestamp { get; set; }
         public List<CommentEntity> comments { get; set; }
         public List<SuggestionEntity> suggestions { get; set; }
         public List<EmployeeEntity> employees { get; set; }
